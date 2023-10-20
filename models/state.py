d510676f5afe04d51e99427c26280025e81a5c15
PSHEMA <p.shema@alustudent.com>
2023-10-20 13:01:19 +0200

    updated state.py

--- models/state.py
+++ models/state.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
-
+import models
 from models.base_model import BaseModel, Base
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
-import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+
+    __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
 
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
+        @property
         def cities(self):
             """ Returns the list of City instances with state_id
             equals to the current State.id. """
